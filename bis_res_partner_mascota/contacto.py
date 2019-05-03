1aa0c96dc3f573e1c3326582a4920c7ffe7b5cb9
gonxi <albertxiri@gmail.com>
2019-05-03 19:27:56 +0200

    Xpath solucionado

--- bis_res_partner_mascota/contacto.py
+++ bis_res_partner_mascota/contacto.py
@@ -1,6 +1,5 @@
 #	-*-	coding:	utf-8	-*-
 from openerp import models, fields, api
 class Contacto(models.Model):
-    _name = 'contacto'
     _inherit = ['res.partner']
-    hacer = fields.Boolean("Lo sé hacer o no")
+    hacer = fields.Boolean(string='Lo sé hacer o no')
